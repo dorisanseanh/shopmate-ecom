7b0f120f27a34950e8a1bf96122693832fc29790
dorisanse <huyenanhbui021021@gmail.com>
2025-04-29 23:44:31 +0700

    create entity role and user

---
+++ src/main/java/com/ecommerce/shopmateecom/entity/Role.java
@@ -0,0 +1,18 @@
+package com.ecommerce.shopmateecom.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+}
---
+++ src/main/java/com/ecommerce/shopmateecom/entity/User.java
@@ -0,0 +1,39 @@
+package com.ecommerce.shopmateecom.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+    private String username;
+    private String password;
+    private String email;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+            , fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+
+}
